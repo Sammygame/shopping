bc41c54e32f2a013da9490df73297651aaf5e475
Sammygame <samvick2020@gmail.com>
2024-09-06 21:47:01 +0100

    Add files via upload

---
+++ products.js
@@ -0,0 +1,53 @@
+const products = [
+  {
+    id: 0,
+    name: "T-shirt 1",
+    price: 29.99,
+    instock: 100,
+    description:
+      "No better way to start your day than with a blue silk Tee from our finest options😘 ",
+    imgSrc: "./img/t1.png",
+  },
+  {
+    id: 1,
+    name: "T-shirt 2",
+    price: 24.99,
+    instock: 43,
+    description:
+      "Red is the colour of love and emanates beauty and confidence ❤️",
+    imgSrc: "./img/t2.png",
+  },
+  {
+    id: 2,
+    name: "T-shirt 3",
+    price: 19.99,
+    instock: 10,
+    description: "Navy blue tees really command the respect you deserve!! ",
+    imgSrc: "./img/t3.png",
+  },
+  {
+    id: 3,
+    name: "T-shirt 4",
+    price: 25.99,
+    instock: 5,
+    description: "How bout a grey and white shirt to start off your week.",
+    imgSrc: "./img/t4.png",
+  },
+  {
+    id: 4,
+    name: "T-shirt 5",
+    price: 29.99,
+    instock: 4,
+    description: "Gradually running out of stock cop yours today.",
+    imgSrc: "./img/t5.png",
+  },
+  {
+    id: 5,
+    name: "T-shirt 6",
+    price: 39.99,
+    instock: 40,
+    description:
+      "Green shirt with polar bear inscription available now for sale.HURRY!!!.",
+    imgSrc: "./img/t6.png",
+  },
+];
